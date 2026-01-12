fcc528f5d6d204ec04e3fbcdcca3e6d5410ddbd1
Malcom-Yingwani <nsovomalcom@gmail.com>
2026-01-12 10:50:55 +0200

    Node.js module unit 0, lesson 1 & 2

---
+++ Unit0/Lesson01/lesson01.js
@@ -0,0 +1,17 @@
+3 + 3
+3/0
+console.log("Hello, Universe");
+console.log("Hello, Universe");
+john wexler
+"Lon Wexler"
+let name = "jon wexler";
+console.log(name);
+
+class Goat2 {
+eat(foodType) {
+console.log(`I love eating ${foodType}`);
+}
+}
+
+let jenna = new Goat2();
+jenna.eat("tin cans");
