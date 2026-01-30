ded77ec3123c33f4bcc415ce1a8900410e63c6c6
Malcom-Yingwani <nsovomalcom@gmail.com>
2026-01-30 11:18:03 +0200

    Unit5, lesson22

---
+++ Unit5/lesson22/recipe_app/controllers/subscribersController.js
@@ -0,0 +1,103 @@
+const Subscriber = require("../models/subscriber");
+const mongoose = require("mongoose");
+
+module.exports = {
+  showSubscribers: async (req, res) => {
+    let subscribers = await Subscriber.find({});
+
+    res.render("subscribers/index", {
+      subscribers: subscribers,
+    });
+  },
+
+  new: (req, res) => {
+    res.render("subscribers/new");
+  },
+
+  create: async (req, res, next) => {
+    try {
+      let subscriberParams = {
+        name: req.body.name,
+        email: req.body.email,
+        zipCode: req.body.zipCode,
+      };
+
+      const subscriber = await Subscriber.create(subscriberParams);
+      res.locals.redirect = "/subscribers";
+      res.locals.subscriber = subscriber;
+      next();
+    } catch (error) {
+      console.log(`Error saving subscriber: ${error.message}`);
+      next(error);
+    }
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    else next();
+  },
+
+  show: async (req, res, next) => {
+    try {
+      let subscriberId = req.params.id;
+      const subscriber = await Subscriber.findById(subscriberId);
+      res.locals.subscriber = subscriber;
+      next();
+    } catch (error) {
+      console.log(`Error fetching subscriber by ID: ${error.message}`);
+      next(error);
+    }
+  },
+
+  showView: (req, res) => {
+    res.render("subscribers/show");
+  },
+
+  edit: async (req, res, next) => {
+    let subscriberId = req.params.id;
+
+    try {
+      let subscriber = await Subscriber.findById(subscriberId);
+      res.render("subscribers/edit", { subscriber: subscriber });
+    } catch (error) {
+      console.log(`Error fetching subscriber by ID: ${error.message}`);
+      next(error);
+    }
+  },
+
+  update: async (req, res, next) => {
+    let subscriberId = req.params.id;
+
+    let subscriberParams = {
+      name: req.body.name,
+      email: req.body.email,
+      zipCode: req.body.zipCode,
+    };
+
+    try {
+      let subscriber = await Subscriber.findByIdAndUpdate(subscriberId, {
+        $set: subscriberParams,
+      });
+      res.locals.redirect = `/subscribers/${subscriberId}`;
+      res.locals.subscriber = subscriber;
+      next();
+    } catch (error) {
+      console.log(`Error updating subscriber by ID: ${error.message}`);
+      next(error);
+    }
+  },
+
+  delete: async (req, res, next) => {
+    let subscriberId = req.params.id;
+
+    try {
+      await Subscriber.findByIdAndDelete(subscriberId);
+      res.locals.redirect = "/subscribers";
+      next();
+    } catch (error) {
+      console.log(`Error deleting subscriber by ID: ${error.message}`);
+      next(error);
+    }
+  },
+};
---
+++ Unit5/lesson22/recipe_app/main.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const homeController = require("./controllers/homeController");
+const errorControllers = require("./controllers/errorControllers");
+const Subscriber = require("./models/subscriber");
+const subscribersController = require("./controllers/subscribersController");
+const usersController = require("./controllers/usersController");
+const coursesController = require("./controllers/coursesController");
+const methodOverride = require("method-override");
+const expressSession = require("express-session");
+const connectFlash = require("connect-flash");
+
+const router = express.Router();
+const app = express();
+
+const layouts = require("express-ejs-layouts");
+
+const mongoose = require("mongoose");
+// setting up mongoose. Use mongoose for more stuctured data. The MongoDB driver for unstructured data
+mongoose.connect("mongodb://localhost:27017/recipe_db");
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("successfully connected to MongoDb using Mongoose!");
+});
+
+// =============== Middleware ==============
+
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+
+app.use(express.json());
+app.use(layouts);
+app.use(express.static("public"));
+router.use(methodOverride("_method", { methods: ["POST", "GET"] })); // we place it before app.use("/", router) so that we first override the method before passing the request to the appropriate route
+
+app.use("/", router);
+
+router.use(
+  expressSession({
+    secret: "secret_passcode",
+    cookie: {
+      maxAge: 4000000, // time in ms
+    },
+    resave: false, // do not resave session if no changes have occured to the session object
+    saveUninitialized: false,
+  }),
+);
+router.use(connectFlash());
+router.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+// ============= routes ============
+
+// Home Routes
+router.get("/", homeController.displayHomePage);
+
+// Course Routes
+router.get("/courses", coursesController.showCourses);
+router.get("/courses/new", coursesController.new);
+router.post(
+  "/courses/create",
+  coursesController.create,
+  coursesController.redirectView,
+);
+router.get("/courses/:id", coursesController.show, coursesController.showView);
+router.get("/courses/:id/edit", coursesController.edit);
+router.put(
+  "/courses/:id/update",
+  coursesController.update,
+  coursesController.redirectView,
+);
+router.delete(
+  "/courses/:id/delete",
+  coursesController.delete,
+  coursesController.redirectView,
+);
+
+// Subscriber Routes
+router.get("/subscribers", subscribersController.showSubscribers);
+router.get("/subscribers/new", subscribersController.new);
+router.post(
+  "/subscribers/create",
+  subscribersController.create,
+  subscribersController.redirectView,
+);
+router.get(
+  "/subscribers/:id",
+  subscribersController.show,
+  subscribersController.showView,
+);
+router.get("/subscribers/:id/edit", subscribersController.edit);
+router.put(
+  "/subscribers/:id/update",
+  subscribersController.update,
+  subscribersController.redirectView,
+);
+router.delete(
+  "/subscribers/:id/delete",
+  subscribersController.delete,
+  subscribersController.redirectView,
+);
+
+// User Routes
+router.get("/users", usersController.index, usersController.indexView);
+router.get("/users/new", usersController.new);
+router.post(
+  "/users/create",
+  usersController.create,
+  usersController.redirectView,
+);
+router.get("/users/:id", usersController.show, usersController.showView);
+router.get("/users/:id/edit", usersController.edit);
+router.put(
+  "/users/:id/update",
+  usersController.update,
+  usersController.redirectView,
+);
+router.delete(
+  "/users/:id/delete",
+  usersController.delete,
+  usersController.redirectView,
+);
+
+app.use(errorControllers.logErrors);
+app.use(errorControllers.respondNoResourceFound);
+app.use(errorControllers.respondInternalError);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running on port: ${app.get("port")}`);
+});
